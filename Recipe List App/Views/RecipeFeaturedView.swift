ac0af89711502bf01352b86f4b3e6bfd0d72e5e6
SamsonShuttle <samsonshuttle@sky.com>
2022-02-23 23:29:27 +0000

    - Add RecipeDetailView - Modify data flow using EnvironmentObject modifer - Create RecipeModel instance in the RecipeTabView

--- Recipe List App/Views/RecipeFeaturedView.swift
+++ Recipe List App/Views/RecipeFeaturedView.swift
@@ -8,8 +8,13 @@
 import SwiftUI
 
 struct RecipeFeaturedView: View {
+    
+    @EnvironmentObject var model:RecipeModel
+    
     var body: some View {
+        
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
     }
 }
 

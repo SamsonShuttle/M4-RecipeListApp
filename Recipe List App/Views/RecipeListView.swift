92025154218a01f6ef5dade28792d8158d5ed276
SamsonShuttle <samsonshuttle@sky.com>
2022-02-28 18:37:12 +0000

    - Change font

--- Recipe List App/Views/RecipeListView.swift
+++ Recipe List App/Views/RecipeListView.swift
@@ -19,7 +19,7 @@ struct RecipeListView: View {
                 Text("All Recipes")
                     .bold()
                     .padding(.top, 40)
-                    .font(.largeTitle)
+                    .font(Font.custom("Avenir Heavy", size: 24))
                 
                 ScrollView {
                     
@@ -43,8 +43,10 @@ struct RecipeListView: View {
                                             
                                             Text(r.name)
                                                 .foregroundColor(.black)
+                                                .font(Font.custom("Avenir Heavy", size: 16))
                                             RecipeHighlights(highlights:  r.highlights)
                                                 .foregroundColor(.black)
+                                            
                                         }
                                     }
                                     
